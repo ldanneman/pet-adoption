3330aed2edbc3a538d99d72a74aaa4237254042e
Lev <levdanneman@gmail.com>
2021-01-15 15:14:27 +0200

    update

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,7 @@ router.post("/register", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   const emailExists = await User.findOne({ email: req.body.email });
-  if (emailExists) return res.status(400).send("Email already exists");
+  if (emailExists) return res.status(400).send("Email already exists";
 
   //Hash the password
   const salt = await bcrypt.genSalt(10);
